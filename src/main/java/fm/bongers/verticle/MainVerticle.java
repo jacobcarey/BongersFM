e09e534cd1f63caf8bbe3f12b060b02210a3f632
Jacob Carey <jacob.oliver.carey@nbrown.co.uk>
2020-11-27 19:35:04 +0000

    BetterBongers all day long! Logging added.

--- src/main/java/fm/bongers/verticle/MainVerticle.java
+++ src/main/java/fm/bongers/verticle/MainVerticle.java
@@ -3,6 +3,7 @@ package fm.bongers.verticle;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Future;
 import io.vertx.core.http.HttpServer;
+import io.vertx.ext.web.Router;
 
 public class MainVerticle extends AbstractVerticle {
 
@@ -16,8 +17,18 @@ public class MainVerticle extends AbstractVerticle {
           req.response().end("OK");
         });
 
-    server.listen(
-        8080,
-        ar -> startFuture.completer().handle(ar.map((Void) null)));
+    server.listen(8080, ar -> startFuture.completer().handle(ar.map((Void) null)));
+
+    Router router = Router.router(vertx);
+
+    router
+        .route("/")
+        .handler(
+            routingContext -> {
+              routingContext
+                  .response()
+                  .putHeader("content-type", "text/html")
+                  .end("Keep on bonging!");
+            });
   }
 }
