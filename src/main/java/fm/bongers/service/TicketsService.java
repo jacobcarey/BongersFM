d641391a3e69e5da5a1be0e724bf271aa63beb03
Jacob Carey <jacob.oliver.carey@nbrown.co.uk>
2021-04-07 23:01:04 +0100

    Added RA ticket check.

--- src/main/java/fm/bongers/service/TicketsService.java
+++ src/main/java/fm/bongers/service/TicketsService.java
@@ -15,6 +15,8 @@ public class TicketsService {
 
   public static final String URL = "https://ra.co/widget/event/1326673/embedtickets";
   public static final int TICKETS_ON_SALE_CURRENTLY = 2;
+  public static final String GENERAL_ADMISSION_WEEKEND = "General Admission Weekend";
+
   private static final Logger LOGGER =
       LoggerFactory.getLogger(LoggerFactory.class); // Required for Logback to work in Vertx
 
@@ -25,19 +27,35 @@ public class TicketsService {
       if (residentAdvisor != null) {
         Elements ticketsOnSale = residentAdvisor.getElementsByClass("onsale");
         if (ticketsOnSale != null && ticketsOnSale.size() > TICKETS_ON_SALE_CURRENTLY) {
-          LOGGER.info("Tickets available.");
-          DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
-          LocalDateTime now = LocalDateTime.now();
-          String timestamp = dtf.format(now);
-          twitterService.sendTweet(
-              "@JacobCarey @Jamestmf @RyanBaines96 Tickets are available, check RA! " + timestamp);
+          sendTicketsAvailableTweet(twitterService);
         } else {
-          LOGGER.info("Tickets not available.");
+          if (ticketsOnSale != null && ticketsOnSale.size() > 0) {
+            for (org.jsoup.nodes.Element element : ticketsOnSale) {
+              if (element.toString() != null
+                  && element
+                      .toString()
+                      .toUpperCase()
+                      .contains(GENERAL_ADMISSION_WEEKEND.toUpperCase())) {
+                sendTicketsAvailableTweet(twitterService);
+              }
+            }
+          } else {
+            LOGGER.info("Tickets not available.");
+          }
         }
       }
 
     } catch (IOException e) {
       e.printStackTrace();
     }
+  }
+
+  private static void sendTicketsAvailableTweet(TwitterService twitterService) {
+    LOGGER.info("Tickets available.");
+    DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+    LocalDateTime now = LocalDateTime.now();
+    String timestamp = dtf.format(now);
+    twitterService.sendTweet(
+        "@JacobCarey @Jamestmf @RyanBaines96 Tickets are available, check RA! " + timestamp);
   }
 }
