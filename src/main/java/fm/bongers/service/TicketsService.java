367df893a55842af6080d4094c279380632937a3
Jacob Carey <jacob.oliver.carey@nbrown.co.uk>
2021-04-07 14:56:25 +0100

    Added RA ticket check.

--- src/main/java/fm/bongers/service/TicketsService.java
+++ src/main/java/fm/bongers/service/TicketsService.java
@@ -8,6 +8,8 @@ import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class TicketsService {
 
@@ -24,8 +26,11 @@ public class TicketsService {
         Elements ticketsOnSale = residentAdvisor.getElementsByClass("onsale");
         if (ticketsOnSale != null && ticketsOnSale.size() > TICKETS_ON_SALE_CURRENTLY) {
           LOGGER.info("Tickets available.");
+          DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+          LocalDateTime now = LocalDateTime.now();
+          String timestamp = dtf.format(now);
           twitterService.sendTweet(
-              "@JacobCarey @Jamestmf @RyanBaines96 Tickets are available, check RA!");
+              "@JacobCarey @Jamestmf @RyanBaines96 Tickets are available, check RA! " + timestamp);
         } else {
           LOGGER.info("Tickets not available.");
         }
