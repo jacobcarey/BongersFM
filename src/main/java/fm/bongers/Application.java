4fc9f742a813fad7d37a0461d8823fdda0c6d0ca
Jacob Carey <jacob.oliver.carey@nbrown.co.uk>
2022-01-20 18:53:07 +0000

    Deployment.

--- src/main/java/fm/bongers/Application.java
+++ src/main/java/fm/bongers/Application.java
@@ -59,8 +59,9 @@ public class Application {
     DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
     LocalDateTime now = LocalDateTime.now();
 
-    LOGGER.info("Sending deployment DM.");
-    String adminUserId = twitterClient.getUserFromUserName("JacobCarey").getId();
+    String adminUserId = twitterClient.getUserFromUserName("@JacobCarey").getId();
+    LOGGER.info("Sending deployment DM to {}.", adminUserId);
+
     twitterClient.postDm("Deployed: " + dtf.format(now), adminUserId);
   }
 }
