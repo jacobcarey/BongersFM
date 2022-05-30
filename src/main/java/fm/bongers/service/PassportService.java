f419ada00a8f9c339656bb1a0f99da7d429687aa
Jacob Carey <jacob.oliver.carey@nbrown.co.uk>
2022-05-30 16:15:15 +0100

    Added passport service.

--- src/main/java/fm/bongers/service/PassportService.java
+++ src/main/java/fm/bongers/service/PassportService.java
@@ -19,6 +19,8 @@ public class PassportService {
       "https://www.passportappointment.service.gov.uk/outreach/publicbooking.ofml";
 
   public static final String NO_APPOINTMENT = "Sorry, there are no available appointments";
+  public static final String BUSY =
+      "Sorry, we're experiencing high demand for this service at the moment and the system is busy. Please try again later.";
 
   private static final Logger LOGGER =
       LoggerFactory.getLogger(LoggerFactory.class); // Required for Logback to work in Vertx
@@ -28,7 +30,8 @@ public class PassportService {
       LOGGER.info("Checking for appointments.");
       Document passportGov = Jsoup.connect(URL).get();
       boolean unavailable =
-          StringUtils.containsIgnoreCase(passportGov.body().text(), NO_APPOINTMENT);
+          StringUtils.containsIgnoreCase(passportGov.body().text(), NO_APPOINTMENT)
+              || StringUtils.containsIgnoreCase(passportGov.body().text(), BUSY);
       if (!unavailable) {
         LOGGER.info("Appointments available!");
         sendAppointmentsAvailableTweet(twitterService);
